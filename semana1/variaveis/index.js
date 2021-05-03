58881e1e2af00b62d71c3ed26da51fdceb02c4a6
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-05-03 17:45:34 -0300

    Aula03-exercicio-variaveis

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,15 @@
+console.log ("Hello")
+console.log("Segunda")
+console.log(2)
+//console.log("AAAAA")
+/*
+const nome = prompt("Qual é seu nome, usuarioquerido")
+console.log(nome)
+*/
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
