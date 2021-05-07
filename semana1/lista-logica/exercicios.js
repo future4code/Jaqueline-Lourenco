b557f122c673c9121eebad8298c9f64b5afaa6db
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-05-07 17:08:47 -0300

    Aula06-primeirParteExercicios

---
+++ semana1/lista-logica/exercicios.js
@@ -0,0 +1,138 @@
+// Exemplos
+
+// Exercício 0A
+/*
+function soma(num1, num2) {
+   // implemente sua lógica aqui
+   return num1 + num2
+}
+*/
+// Exercício 0B
+/*
+function imprimeMensagem() {
+   // implemente sua lógica aqui
+   const mensagem = prompt('Digite uma mensagem!')
+
+   console.log(mensagem)
+}
+*/
+// Exercícios
+
+//Exercício 1
+/*
+function calculaAreaRetangulo() {
+   // implemente sua lógica aqui
+   let altura = prompt("Insira a altura")
+   let largura = prompt("Insira a largura")
+   const area = altura * largura
+   console.log(area)
+}
+   calculaAreaRetangulo()
+*/
+//Exercício 2
+/*
+function imprimeIdade() {
+   // implemente sua lógica aqui
+   let anoAtual = Number(prompt("Insira o ano Atual?"))
+   let anoNascimento = Number(prompt("Insira o ano Nscimento?"))
+   let idadeAtual = anoAtual - anoNascimento
+   console.log(idadeAtual)
+
+}
+  imprimeIdade ()
+*/
+//Exercício 3
+
+function calculaIMC(peso, altura) {
+   // implemente sua lógica aqui
+ return peso / (altura * altura)
+}
+  calculaIMC(85, 1.8)
+
+//Exercício 4
+/*
+function imprimeInformacoesUsuario() {
+      // implemente sua lógica aqui
+      let  nome = prompt("Digite seu nome aqui")
+      let  idade = Number(prompt("Digite sua Idade"))
+      let  email = prompt("Digite seu e-mail")
+      
+      console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+
+   }
+     imprimeInformacoesUsuario()
+*/
+//Exercício 5
+
+function imprimeTresCoresFavoritas() {
+   // implemente sua lógica aqui
+   let cor1 = prompt("Digite sua primeira cor favorita")
+   let cor2 = prompt("Digite sua segunda cor favorita")
+   let cor3 = prompt("Digite sua terceira cor favorita")
+   
+   let cores = [cor1, cor2, cor3]
+
+   let 
+   console.log(cores)
+}
+  imprimeTresCoresFavoritas()
+
+//Exercício 6
+
+function retornaStringEmMaiuscula(string) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 7
+
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 8
+
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+}
+
+// Exercício 9
+
+function retornaPrimeiroElemento(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 10
+
+function retornaUltimoElemento(array) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 11
+
+function trocaPrimeiroEUltimo(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 12
+
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 13
+
+function checaRenovacaoRG() {
+   // implemente sua lógica aqui
+}
+
+// Exercício 14
+
+function checaAnoBissexto(ano) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 15
+
+function checaValidadeInscricaoLabenu() {
+   // implemente sua lógica aqui
+}
