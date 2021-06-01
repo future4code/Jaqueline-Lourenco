2f7948087fe139a0c231d864971a02225f9c6990
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-06-01 19:00:35 -0300

    Componentes Funcionais, props e styled-components

---
+++ semana5/Componentes-Funcionais/labedin/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem=".\labedin\src\components\CardGrande\img\AirBrush_20210111155648.jpg" 
+          nome="Jaqueline Lourenço" 
+          descricao="Oi, sou a Jaqueline. Sou estudante da Labenu. Amo esta em casa, sou apaixonada em tecnologia saber mais sobre novos formas da tecnologia, e gosto de assistir filmes e series."
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        />
+        
+        <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
