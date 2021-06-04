fc371aeabdc7f3f7b6d7edf0cb388a417785450b
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-06-04 17:20:50 -0300

    Finalização-Labedin

--- semana5/Componentes-Funcionais/labedin/src/App.js
+++ semana5/Componentes-Funcionais/labedin/src/App.js
@@ -1,41 +1,82 @@
 import React from 'react';
-import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
+import ImgPerfil from './components/CardGrande/img/ImgPerfil.jpg';
+import CardPequeno from './components/CardPequeno/CardPequeno';
 import ImagemButton from './components/ImagemButton/ImagemButton';
+import styled from "styled-components";
+import {createGlobalStyle} from "styled-components";
+import EmailIcon from '@material-ui/icons/Email';
+import PersonPinCircleIcon from '@material-ui/icons/PersonPinCircle';
+import WorkIcon from '@material-ui/icons/Work';
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  h2 {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+
+`;
+
+const AppContainer = styled.div `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+`;
+
+const PageSectionContainer = styled.div`
+  width: 40vw;
+  margin: 10px 0;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="page-section-container">
+    <AppContainer>
+      <GlobalStyle/>
+      <PageSectionContainer>
         <h2>Dados pessoais</h2>
         <CardGrande 
-          imagem=".\labedin\src\components\CardGrande\img\AirBrush_20210111155648.jpg" 
+          imagem = { ImgPerfil }
           nome="Jaqueline Lourenço" 
-          descricao="Oi, sou a Jaqueline. Sou estudante da Labenu. Amo esta em casa, sou apaixonada em tecnologia saber mais sobre novos formas da tecnologia, e gosto de assistir filmes e series."
+          descricao="Oi, sou a Jaqueline. Sou estudante da Labenu. Amo esta em casa, sou apaixonada em tecnologia e abrir e resolver tal problema em relação, e gosto de assistir filmes e series."
         />
-        
         <ImagemButton 
           imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
           texto="Ver mais"
         />
+
+      </PageSectionContainer>
+
+      <div>
+        <CardPequeno iconEmail={<EmailIcon/>} email={"jaquelineloure50@gmail.com"}/>
+        <CardPequeno iconEndereco={<PersonPinCircleIcon/>} endereco={"Rua antiga 10, nº94"}/>
       </div>
 
-      <div className="page-section-container">
+      <PageSectionContainer>
         <h2>Experiências profissionais</h2>
         <CardGrande 
           imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
           nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          descricao="Estudante" 
         />
         
         <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+          iconWork={<WorkIcon/>}
+          nome="Calvin Klein" 
+          descricao="Estoquista" 
         />
-      </div>
 
-      <div className="page-section-container">
+      </PageSectionContainer>
+
+      <PageSectionContainer>
         <h2>Minhas redes sociais</h2>
         <ImagemButton 
           imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
@@ -46,8 +87,8 @@ function App() {
           imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
           texto="Twitter" 
         />        
-      </div>
-    </div>
+      </PageSectionContainer>
+    </AppContainer>
   );
 }
 
--- semana5/Componentes-Funcionais/labedin/src/components/CardGrande/CardGrande.js
+++ semana5/Componentes-Funcionais/labedin/src/components/CardGrande/CardGrande.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import './CardGrande.css'
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 200px;
+
+.icon{
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+}
+`;
+const CardImg = styled.img`
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+`;
+
+const BigText = styled.h4`
+    margin-bottom: 15px;
+`;
+
+const BigCardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+`;
 
 function CardGrande(props) {
     return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
+        <CardContainer>
+            <CardImg src={ props.imagem } />
+            <p className="icon">{props.iconWork}</p>
+            <BigCardContainer>
+                <BigText>{ props.nome }</BigText>
                 <p>{ props.descricao }</p>
-            </div>
-        </div>
+            </BigCardContainer>
+        </CardContainer>
     )
 }
 
