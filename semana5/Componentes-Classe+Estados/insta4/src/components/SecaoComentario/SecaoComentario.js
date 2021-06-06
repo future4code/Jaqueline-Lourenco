d368b23f50f723370d8e351ec4bbf7bb011d8e34
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-06-06 20:31:32 -0300

    finalizaçao

--- semana5/Componentes-Classe+Estados/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/Componentes-Classe+Estados/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -1,34 +1,26 @@
 import React, {Component} from 'react'
-import styled from 'styled-components'
+import './SecaoComentario.css'
 
-const CommentContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    padding: 5px;
-`
-
-const InputComentario = styled.input`
-    width: 100%;
-    margin-right: 5px;
-`
 
 export class SecaoComentario extends Component {
 	state = {
-
+		valorInputComentario: ''
 	}
 
-	onChangeComentario() {
-
+	onChangeComentario = (event) => {
+		console.log(event.target.value)
+	this.setState({valorInputComentario: event.target.value})
 	}
 
 	render() {
-		return <CommentContainer>
-			<InputComentario
-				placeholder={'Comentário'}
-				value={''}
+		return <div className={'comment-container'}>
+			<input
+				className={'input-comentario'}
+				value={'Comentário'}
+				value={this.state.valorInputComentario}
 				onChange={this.onChangeComentario}
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
-		</CommentContainer>
+		</div>
 	}
 }
