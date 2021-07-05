4d3639d067cc021b29ac70f2aac61b9c391e0cff
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-07-05 18:55:10 -0300

    Criando app acrescentando pages

---
+++ semana10/ProjLabex/labex-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeTwoToneIcon from '@material-ui/icons/HomeTwoTone';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeTwoToneIcon />
+    </div>
+  );
+}
+
+export default App;
