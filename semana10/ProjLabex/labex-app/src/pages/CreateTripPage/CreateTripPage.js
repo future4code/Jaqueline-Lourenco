1475f5ca6d873e4290bd12da8583542cac108cd4
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-07-20 17:56:31 -0300

    Atualizando

--- semana10/ProjLabex/labex-app/src/pages/CreateTripPage/CreateTripPage.js
+++ semana10/ProjLabex/labex-app/src/pages/CreateTripPage/CreateTripPage.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import {useHistory} from 'react-router-dom';
+import React, { useState } from 'react'
 import {GlobalStyled, Container} from '../../styled';
 import Logo from '../../assets/logo.jpg'
-import {useProtectedPage} from '../../hooks/useProtectedPage'
-import axios from 'axios';
-import {useForm} from '../../hooks/useForm'
 import { TextField, Button } from '@material-ui/core'
 import { KeyboardDatePicker } from '@material-ui/pickers';
+import { useForm } from '../../hooks/useForm';
+import axios from 'axios'
+import { useHistory } from 'react-router-dom';
+import { useProtectedPage } from '../../hooks/useProtectedPage';
 
 export default function CreateTrip () {
   const [form, onChangeInput] = useForm({
@@ -19,22 +19,20 @@ export default function CreateTrip () {
   useProtectedPage()
 
   const history = useHistory()
-  
+
+  const [date, setDate] = useState(new Date())
+
   const goToHome = () => {
     history.push("/")
-  }
-  const goToListTrips = () => {
-    history.push("/list")
   }
   const goToTripDetail = () => {
     history.push("/detail")
   }
 
-  const [date, setDate] = useState(new Date())
   const onSubmitForm = (event) => {
     event.preventDefault()
     const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
-  
+
     const body = {
       name: form.name,
       planet: form.planet,
@@ -42,59 +40,61 @@ export default function CreateTrip () {
       description: form.description,
       durationInDays: form.duration
     }
+
     axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labeX/jaqueline-lourenco-Molina/trips', body, {
       headers: {
         auth: window.localStorage.getItem('token')
       }
+    }).then((response) => {
+      history.push('/list')
     })
   }
   return (
-    <div>
-      <GlobalStyled>
-        <img src={Logo}/>
-        Create Trip
-        <div>
-          <button onClick={goToHome}>Home</button>
-          <button onClick={goToListTrips}>List</button>
-          <button onClick={goToTripDetail}>Detail Trip</button>
-        </div>
-      </GlobalStyled>
-      <Container onSubmit={onSubmitForm}>
-        <TextField
-          label={'Nome'} 
-          onChange={onChangeInput}
-          name={'name'}
-          value={form['name']}
-        />
-        <TextField
-          label={'Planeta'}
-          onChange={onChangeInput}
-          name={'planet'}
-          value={form['planet']}
-        />
-        <KeyboardDatePicker
+  <div>
+    <GlobalStyled>
+      <img src={Logo}/>
+      Create Trip
+      <div>
+        <button onClick={goToHome}>Home</button>
+        <button onClick={goToTripDetail}>Detail Trip</button>
+      </div>
+    </GlobalStyled>
+    <Container onSubmit={onSubmitForm}>
+      <TextField
+        label={'Name'} 
+        onChange={onChangeInput}
+        name={'name'}
+        value={form['name']}
+      />
+      <TextField
+        label={'Planet'}
+        onChange={onChangeInput}
+        name={'planet'}
+        value={form['planet']}
+      />
+      <KeyboardDatePicker
           disableToolbar
           variant="inline"
           format="dd/MM/yyyy"
           margin="normal"
-          label="Data"
+          label="Date"
           value={date}
           onChange={date => setDate(date)}
         />
-        <TextField
-          label={'Descrição'}
-          onChange={onChangeInput}
-          name={'description'}
-          value={form['description']}
-        />
-        <TextField
-          label={'Duração em dias'} type={'number'}
-          onChange={onChangeInput}
-          name={'duration'}
-          value={form['duration']}
-        />
-        <Button type={'submit'}>Create</Button>
-      </Container>
-    </div>
-  );
+      <TextField
+        label={'Description'}
+        onChange={onChangeInput}
+        name={'description'}
+        value={form['description']}
+      />
+      <TextField
+        label={'Duration in days'} type={'number'}
+        onChange={onChangeInput}
+        name={'duration'}
+        value={form['duration']}
+      />
+      <Button variant={'contained'} color={'primary'} type={'submit'}>Create</Button>
+    </Container>
+  </div>
+  )
 }
