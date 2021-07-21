3e8902d860bc8fabf0bc9e8efef1d098bdff4ec5
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-07-20 21:49:26 -0300

    Começando adicionando pastas

---
+++ semana12/Projeto/labeddit-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeIcon from '@material-ui/icons/Home';
+
+export default function App () {
+  return (
+    <div className="App">
+      
+      <h1>LabEddit</h1>
+      <HomeIcon />
+
+    </div>
+  );
+}
